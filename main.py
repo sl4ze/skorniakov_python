1fc071db3c9940754bf0a18fa63474642059a7c9
Skorniakov Konstantin <slaze_@outlook.com>
2025-03-24 17:43:16 +0400

    fix spelling add coment

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def run():
     while True:
         print('enter a number or name, ctrl+c to exit')
         var = input()
-        try: 
+        try:
             numeric_var = int(var)
             if numeric_var > 7:
                 print('Hello')
@@ -19,11 +19,11 @@ def run():
         while True:
             numlist = input().split()
             try:
-                for number in set((numlist)):
+                for number in set((numlist)): #filter non-unique elemts
                     number = float(number)
                     if number % 3 == 0:
                         mltpls3.append(number)
-                print('ay elements that are multiples of 3 : ', mltpls3)
+                print('unique list elements that are multiples of 3 : ', mltpls3)
                 break
             except Exception as e:
                 print('Wrong input: ', e)
