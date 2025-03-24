cbf0a8a738b624c3da055a4e7404c8211d60f2a3
Skorniakov Konstantin <slaze_@outlook.com>
2025-03-24 13:29:02 +0400

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+
+def run():
+    while True:
+        print('enter a number or name, ctrl+c to exit')
+        var = input()
+        try: 
+            numeric_var = int(var)
+            if numeric_var > 7:
+                print('Hello')
+        except:
+            
+            if var == 'John':
+                print('Hello, John')
+            else:
+                print('There is no such name')
+        print('enter a numeric array, numbers are separated by space only')
+        
+        mltpls3 = []
+        while True:
+            numlist = input().split()
+            try:
+                for number in set((numlist)):
+                    number = float(number)
+                    if number % 3 == 0:
+                        mltpls3.append(number)
+                print('ay elements that are multiples of 3 : ', numlist)
+                break
+            except Exception as e:
+                print('Wrong input: ', e)
+
+
+
+
+
+if __name__ == '__main__':
+    run()
